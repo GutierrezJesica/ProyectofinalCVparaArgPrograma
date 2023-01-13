8ee70f43f38f4898233715f9345cdbbfd4f25571
Jesica Gutierrez <jesica89@gmail.com>
2023-01-13 20:34:37 -0300

    proyecto final

---
+++ css/estilo2.css
@@ -0,0 +1,83 @@
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300;1,400&display=swap');
+body {
+    font-family: open, sans-serif;
+    margin: 0;
+}
+.degrade {
+    background: linear-gradient(180deg, rgb(230, 230, 230), rgb(255, 255, 255));
+    margin: 0px 0px 200px 80px;
+    max-width: 100%;
+    height: auto;
+}
+.grilla {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 30px;
+    padding: 50px 50px 0px 50px;
+}
+.intro {
+    display: block;
+    margin: auto;
+}
+.nombres {
+    font-family: "Roboto";
+    font-weight:400;
+    font-size: 50px;
+    margin-bottom: 15px;
+}
+.bajada {
+    font-family: "Roboto";
+    font-weight: 400;
+    font-size: 25px;
+    margin-bottom: 20px;
+}
+.descripcion {
+    font-family: "Roboto";
+    font-weight: 300;
+    font-size: 15px;
+    margin-bottom: 20px;
+}
+.icono {
+    max-width: 30px;
+    padding: 20px 10px 30px 0px;
+}
+.perfil {
+    max-width: 100%;
+    height: auto;
+}
+.caja {
+    background: linear-gradient(180deg, rgb(230, 230, 230), rgb(255, 255, 255));
+    border-radius: 20px;
+    height: auto;
+    max-width: 90%;
+    padding: 50px;
+    margin: 50px 20px 200px 80px;
+}
+.titulo {
+    font-family: "Roboto";
+    font-weight: 400;
+    font-size: 25px;
+    margin:0;
+    
+}
+.text {
+    font-family: "Roboto";
+    font-weight: 300;
+    font-size: 15px;
+    margin: 0;
+}
+.botonera {
+    position: fixed;
+    left: 10px;
+    top: 10px;
+}
+.boton {
+    width: 50px;
+    display: block;
+    padding: 5px;
+}
+.vacio {
+    max-width: 90%;
+    height: 200px;
+    margin: 200px;
+}
---
+++ java script/eventos.js
@@ -0,0 +1,62 @@
+function enAmarillo1() {
+Element =document.getElementById("pers");
+Element.src="img/personal2.png";
+}
+function enNegro1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal.png";
+}
+
+function enAmarillo2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral2.png";
+}
+ function enNegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.png";
+}
+
+function enAmarillo3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades2.png";
+}
+function enNegro3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.png";
+}
+
+function enAmarillo4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function enNegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
+
+function enAmarillo5() {
+    Element =document.getElementById("be");
+    Element.src="img/behance2.png";
+}
+function enNegro5() {
+    Element =document.getElementById("be");
+    Element.src="img/behance.png";
+}
+
+function enAmarillo6() {
+    Element =document.getElementById("gi");
+    Element.src="img/github2.png";
+}
+function enNegro6() {
+    Element =document.getElementById("gi");
+    Element.src="img/github.png";
+}
+
+function enAmarillo7() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin2.png";
+}
+function enNegro7() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin.png";
+}
